a21aefbd4462aadaf51be8c0519e97edd0d4816e
Oscar Jove <turkin@gmail.com>
2022-11-07 20:49:33 -0300

    CRUD de libros

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/api/clients', (req, res, next) => {
 app.get('/api/clients/:id', (req, res, next) => {
     //Respondo clientes
     MongoClient.connect(url, function (err, db) {
-        //if (err) throw err;
+        if (err) throw err;
         var dbo = db.db("library");
         var query = { dni:Number(req.params.id)};
         console.log(query);
@@ -122,15 +122,10 @@ app.patch('/api/clients/:id', (req, res, next) => {
         });
 
     });
-   
-
 });
-    
-
-
 
 app.delete('/api/clients/', (req, res, next) => {
-    res.status(400).json('');
+    res.status(404).json('');
 });
 
 app.delete('/api/clients/:id', (req, res, next) => {
@@ -142,7 +137,6 @@ app.delete('/api/clients/:id', (req, res, next) => {
         var query = { dni: Number(req.params.id) };
         dbo.collection("clients").findOne(query, function (err, result) {
             if (err) throw err;
-            //db.close();
             if (result) {
                 var borrado = result;
                 dbo.collection("clients").deleteOne(query, function (err, obj) {
@@ -165,27 +159,145 @@ app.delete('/api/clients/:id', (req, res, next) => {
 //CRUD libros
 
 app.get('/api/books', (req, res, next) => {
-    res.send('Respondo libros');
+    //Respondo libros
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("library");
+        dbo.collection("books").find({}).toArray(function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            res.json(result);
+        });
+    });
+});
+
+app.get('/api/books/:id', (req, res, next) => {
+    //Respondo libros
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("library");
+        var query = { id: Number(req.params.id) };
+        console.log(query);
+        dbo.collection("books").findOne(query, function (err, result) {
+            if (err) throw err;
+            console.log(result);
+            db.close();
+            if (result) {
+                res.json(result);
+            } else {
+                res.status(404).json('');
+            }
+        });
+    });
 });
 
 app.post('/api/books', (req, res, next) => {
-    res.send('Ingreso libro y respondo con los datos ingresados');
+    //Ingreso libro y respondo con los datos ingresados
+    console.log(req.body);
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("library");
+        dbo.collection("books").insertOne(req.body, function (err, res) {
+            if (err) throw err;
+            console.log("Libro agregado");
+            db.close();
+        });
+    });
+    res.status(201).send(req.body);
 });
 
 app.patch('/api/books/', (req, res, next) => {
-    res.status(400).send('No detalla libro a actualizar');
+    res.status(400).send('');
 });
 
 app.patch('/api/books/:id', (req, res, next) => {
-    res.send('Actualizo libro ' + req.params.id + ' y respondo con los datos actualizados');
+    //Actualizo libro 
+    var query = { id: Number(req.params.id) };
+    var changed = 0;
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("library");
+        if (typeof req.body.autor !== 'undefined' && req.body.autor) {
+            var newvalues = { $set: { autor: req.body.autor } };
+            dbo.collection("books").updateOne(query, newvalues, function (err, res) {
+                if (err) throw err;
+                if (res.modifiedCount) {
+                    console.log("Autor actualizado");
+                    changed = changed + 1;
+                }
+            });
+        }
+        if (typeof req.body.nombre !== 'undefined' && req.body.nombre) {
+            var newvalues = { $set: { nombre: req.body.nombre } };
+            dbo.collection("books").updateOne(query, newvalues, function (err, res) {
+                if (err) throw err;
+                if (res.modifiedCount) {
+                    console.log("Nombre actualizado");
+                    changed = changed + 1;
+                }
+            });
+        }
+        if (typeof req.body.id !== 'undefined' && req.body.id) {
+            var newvalues = { $set: { id: req.body.id } };
+            dbo.collection("books").updateOne(query, newvalues, function (err, res) {
+                if (err) throw err;
+                if (res.modifiedCount) {
+                    console.log("ID actualizado");
+                    changed = changed + 1;
+                }
+            });
+        };
+        //respondo con los datos actualizados
+        if (typeof req.body.id !== 'undefined' && req.body.id) {
+            var newQuery = { id: Number(req.body.id) };
+        } else {
+            var newQuery = { id: Number(req.params.id) };
+        }
+        dbo.collection("books").findOne(newQuery, function (err, result) {
+            if (err) throw err;
+            db.close();
+            if (result && changed) {
+                res.json(result);
+            } else if (result && !changed) {
+                res.status(400).json('');
+            } else {
+                res.status(404).json('');
+            }
+        });
+
+    });
 });
 
 app.delete('/api/books/', (req, res, next) => {
-    res.status(400).send('No detalla libro a eliminar');
+    res.status(404).send('');
 });
 
 app.delete('/api/books/:id', (req, res, next) => {
-    res.send('Elimino libro ' + req.params.id + ' y respondo con los datos eliminados');
+    //Elimino libro y respondo con los datos eliminados');
+    var borrado;
+    MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("library");
+        var query = { id: Number(req.params.id) };
+        dbo.collection("books").findOne(query, function (err, result) {
+            if (err) throw err;
+            if (result) {
+                var borrado = result;
+                dbo.collection("books").deleteOne(query, function (err, obj) {
+                    if (err) throw err;
+                    console.log("Libro eliminado");
+                    db.close();
+                    console.log(borrado);
+                    res.json(borrado);
+                });
+            } else {
+                db.close();
+                res.status(404).json('');
+
+            }
+        });
+    });
 });
 
 
